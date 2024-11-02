9bcf1bbb3113403a8474d0cbe6fc17b8ddfaca79
dbehshhhej <82349684+dbehshhhej@users.noreply.github.com>
2024-11-02 11:18:32 -0500

    Upgrade to API Calls
    - Added a Historical Data API Call File
    - Renamed future_forecast to future_forecast_call, for clarity
    - For all three files, fixed return to return the JSON Object

--- historical_data_call.js
+++ historical_data_call.js
@@ -1,13 +1,14 @@
-export function getFutureForecast(lat, lon)
+export function getHistoricalData(lat, lon, unixTC)
 {
     console.log(lat);
     console.log(lon);
     $.ajax({
         method: 'GET',
+        url: 'https://api.openweathermap.org/data/3.0/onecall/timemachine?lat='+lat+'&lon='+lon+'&dt='+unixTC+'&units=imperial',
         headers: { 'appid': '6785ce768440fe770c2b2f54dc298527'},
         contentType: 'application/json',
         success: function(result) {
-            console.log(result);
+            return result;
         },
         error: function ajaxError(jqXHR) {
             console.error('Error: ', jqXHR.responseText);
