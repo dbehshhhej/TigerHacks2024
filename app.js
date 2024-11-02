506354aac0b750c29444eda7187dcb67bd777626
Elijah VanDyne <114081115+ELVanDyne@users.noreply.github.com>
2024-11-02 09:23:53 -0500

    Messed around

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //function for dropdown button
+/*
 function toggleDropdown() {
 
     const dropdown = document.getElementById("dropdown");
@@ -27,3 +28,25 @@ window.onclick = function(event) {
         }
     }
 }
+*/
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+function filterFunction() {
+    const input = document.getElementById("myInput");
+    const filter = input.value.toUpperCase();
+    const div = document.getElementById("myDropdown");
+    const a = div.getElementsByTagName("a");
+    for (let i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
+
