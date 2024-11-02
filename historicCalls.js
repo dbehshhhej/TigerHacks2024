a68bc9ef7f7b88e9467d7a09441d4d2e19833e1c
Elijah VanDyne <114081115+ELVanDyne@users.noreply.github.com>
2024-11-02 13:21:57 -0500

    Still not workin

--- historicCalls.js
+++ historicCalls.js
@@ -8,6 +8,29 @@ function saveHistoricalData(cityName, state, plantDate)
     console.log(data);
     console.log(latitude);
     console.log(longitude);
+    
+    let data2 = {
+        "cities": {
+            cityName: {
+                "lat": latitude,
+                "lon": longitude,
+                "dates": {
+                    "1633046400": { // Unix time for 2021-10-01
+                        "maxTemp": 20,
+                        "minTemp": 10
+                    },
+                    "1633132800": { // Unix time for 2021-10-02
+                        "maxTemp": 22,
+                        "minTemp": 12
+                    }
+                }
+            },
+            
+        }
+    };
+    localStorage.setItem("GDD_data", JSON.stringify(data2));
+
+
 
     var stored = JSON.parse(localStorage.getItem("GDD_data"));
     
@@ -35,3 +58,15 @@ function saveHistoricalData(cityName, state, plantDate)
     }
     
 }
+document.getElementById('getGeocode').addEventListener('click', function() {
+    const city = document.getElementById('cityInput').value;
+    const state = document.getElementById('stateInput').value;
+    getCoordinates(city, state);
+});
+
+document.getElementById('textCalls').addEventListener('click', function() {
+    const city = document.getElementById('cityInput').value;
+    const state = document.getElementById('stateInput').value;
+    const plantDate = document.getElementById('plantDate').value;
+    saveHistoricalData(city, state, plantDate);
+});
