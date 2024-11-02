5ec2eb7f04cc20fadf40bf6cd639d5956f8003c2
connerberry <cmb2vb@mail.missouri.edu>
2024-11-02 10:45:34 -0500

    Constants in separate file, import functions

---
+++ constants.js
@@ -0,0 +1,27 @@
+// Base temps of crops
+export const baseTemps = {
+  corn: 50, // Base GDD 50°F
+  soybean: 50, // Base GDD 50°F
+  wheat: 40, // Base GDD 40°F
+  alfalfa: 41, // Base GDD 41°F
+  cotton: 60, // Base GDD 60°F
+  tomato: 50, // Base GDD 50°F
+  lettuce: 40, // Base GDD 40°F
+  cucumber: 55, // Base GDD 55°F
+  rice: 50, // Base GDD 50°F
+  potato: 45, // Base GDD 45°F
+};
+
+// Emergence GDD of crops
+export const emergenceGDD = {
+  corn: 90, // Approx. 90 GDDs for corn to emerge
+  soybean: 120, // Approx. 120 GDDs for soybean
+  wheat: 180, // Approx. 180 GDDs for wheat
+  alfalfa: 200, // Approx. 200 GDDs for alfalfa
+  cotton: 200, // Approx. 200 GDDs for cotton
+  tomato: 150, // Approx. 150 GDDs for tomato
+  lettuce: 120, // Approx. 120 GDDs for lettuce
+  cucumber: 150, // Approx. 150 GDDs for cucumber
+  rice: 100, // Approx. 100 GDDs for rice
+  potato: 120, // Approx. 120 GDDs for potato
+};
--- script.js
+++ script.js
@@ -8,33 +8,10 @@ let calculateButton = document.querySelector("#calculate-button");
 
 const daysSincePlanting = 8; // Number of days prior the present the crop was planted. Will be removed later, since the current date will be used (and days since planting is found between the planting date and the present).
 
-// Base temps of crops
-const baseTemps = {
-  corn: 50, // Base GDD 50°F
-  soybean: 50, // Base GDD 50°F
-  wheat: 40, // Base GDD 40°F
-  alfalfa: 41, // Base GDD 41°F
-  cotton: 60, // Base GDD 60°F
-  tomato: 50, // Base GDD 50°F
-  lettuce: 40, // Base GDD 40°F
-  cucumber: 55, // Base GDD 55°F
-  rice: 50, // Base GDD 50°F
-  potato: 45, // Base GDD 45°F
-};
-
-// Emergence GDD of crops
-const emergenceGDD = {
-  corn: 90, // Approx. 90 GDDs for corn to emerge
-  soybean: 120, // Approx. 120 GDDs for soybean
-  wheat: 180, // Approx. 180 GDDs for wheat
-  alfalfa: 200, // Approx. 200 GDDs for alfalfa
-  cotton: 200, // Approx. 200 GDDs for cotton
-  tomato: 150, // Approx. 150 GDDs for tomato
-  lettuce: 120, // Approx. 120 GDDs for lettuce
-  cucumber: 150, // Approx. 150 GDDs for cucumber
-  rice: 100, // Approx. 100 GDDs for rice
-  potato: 120, // Approx. 120 GDDs for potato
-};
+import { baseTemps, emergenceGDD } from "./constants.js";
+import { getCoordinates } from "./latlong_converter.js";
+import { projectDaysRemaining } from "./linear_projection.js";
+import { getFutureForecast } from "./future_forecast.js";
 
 // let lat = 2389752; // Temp vals, set from function calls
 // let long = 1492385702;
