81d59343a7f5a870ce561d46d0208289b1fd6aac
lanserman <lanejohnson450@gmail.com>
2024-11-01 21:15:51 -0500

    random update

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 //function for dropdown button
 function toggleDropdown() {
+
     const dropdown = document.getElementById("dropdown");
+
     dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
 }
 
 function selectCity(city) {
+
     document.getElementById("selectCityDisplay").textContent = "Selected City: " + city;
+
     toggleDropdown();
 }
 
 window.onclick = function(event) {
+
     const dropdown = document.getElementById("citylist");
+
     const button = document.querySelector(".dropdown-button");
+
     if (!event.target.matches('.dropdownbutton')) {
+
         if (!button.contains(event.target) && dropdown.style.display === "block") {
+            
             dropdown.style.display = "none";
         }
     }
